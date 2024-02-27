0052c76111c516ad4a3b50880e00604ed46da104
Rushil57 <>
2024-02-27 17:05:02 +0530

    Changes in CloseToBook feature

---
+++ SCE.Web/Models/SCEViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SCE.Web.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCE.Web.Models
+{
+    public class SCEViewModel
+    {
+        public List<StarsViewModel> StarsViewModel { get; set; }
+        public List<CloseToBookDto> CloseToBook { get; set; }
+        public List<SalesCycleExtensionDto> SalesCycleExtension { get; set; }
+        public List<SaleSlapseDaysDto> SaleSlapseDays { get; set; }
+    }
+}
